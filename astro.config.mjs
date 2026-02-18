017ec80e8b467c4ff3c4676bc444c818aec69fa3
Mike <bill@Michaels-MacBook-Pro.local>
2026-02-18 14:37:59 -0500

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import netlify from "@astrojs/netlify";
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: netlify(),
 });
